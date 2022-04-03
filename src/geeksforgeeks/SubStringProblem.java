7486952e57d317c6c3c5b9b876f4aae8d9e59578
Bhingare <nanasaheb.bhingare@hp.com>
2022-04-03 14:14:21 +0530

    New Solution

--- src/geeksforgeeks/SubStringProblem.java
+++ src/geeksforgeeks/SubStringProblem.java
@@ -5,17 +5,16 @@ public class SubStringProblem {
 		String str = "abc";
 		int arrayLength = str.toCharArray().length;
 		System.out.println("SubString Cnt :- " + (arrayLength * (arrayLength + 1) / 2));
-		/*for (int i = 0; i < str.length(); i++) {
-			for (int j = i+1; j <= str.length(); j++) {
-				System.out.println(str.substring(i, j));
-			}
-		}*/
+		/*
+		 * for (int i = 0; i < str.length(); i++) { for (int j = i+1; j <=
+		 * str.length(); j++) { System.out.println(str.substring(i, j)); } }
+		 */
 		char[] strArray = str.toCharArray();
 		for (int i = 0; i < arrayLength; i++) { // 3 time executed
-			for (int j = i; j < arrayLength; j++) { //
+			for (int j = (i + 1); j <= arrayLength; j++) { //
 				String strOutput = "";
-				for (int k = j; k < arrayLength; k++) {
-					strOutput+=strArray[k];
+				for (int k = i; k < j; k++) {
+					strOutput += strArray[k];
 				}
 				System.out.println(strOutput);
 			}
